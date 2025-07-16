1484501e672b3b86ad3b345843276df2b311bdcc
Walker Frederick <walker@urdev.xyz>
2025-07-16 16:19:41 -0400

    rn project setup

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
